a91adb08012d77eafc933c711c427030af0c909f
mugunthan17 <mugunthannagalingam17@gmail.com>
2025-05-25 22:39:07 +0530

    altered server.js

--- server.js
+++ server.js
@@ -45,8 +45,9 @@ app.post('/merge', upload.fields([
     fs.mkdirSync(outputDir);
   }
 
-  const cmd = `ffmpeg -y -loop 1 -framerate 25 -t 10 -i uploads/07cbe355fd7ed9c99b94eeb898f57764 \
--i uploads/c060e43d7962782820b0eaacc964c63d \
+  const cmd = `ffmpeg -y -loop 1 -framerate 25 -t 10 \
+-i uploads/07cbe355fd7ed9c99b94eeb898f57764.png \
+-i uploads/c060e43d7962782820b0eaacc964c63d.mp3 \
 -c:v libx264 -c:a aac -b:a 192k -shortest -pix_fmt yuv420p \
 /app/outputs/output-1748192639735.mp4`;
 
